8b2e253bdf6ce0176f153e8b90778e7a1732ad3b
mahesh <maheshharangule02@yahoo.com>
2020-12-22 16:22:09 +0530

    final commit

--- src/Google.java
+++ src/Google.java
@@ -1,7 +1,5 @@
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class Google {
 
@@ -11,17 +9,6 @@ public class Google {
 	    driver.navigate().to("http://www.google.com/"); 
 	    System.out.println(driver.getTitle());
 	    driver.close();
-	    driver.quit();
-	    driver.close();
-	    driver.findElement(By.cssSelector(""));
-	    driver.findElement(By.cssSelector(""));
-	    driver.findElement(By.cssSelector("hgfjgfjgjgf"));
-	    driver.findElement(By.cssSelector(""));
-	    driver.findElement(By.cssSelector(""));
-	    driver.findElement(By.cssSelector("hgfjgfjgjgf"));
-	    driver.findElement(By.cssSelector(""));
-	    driver.findElement(By.cssSelector(""));
-	    driver.findElement(By.cssSelector("hgfjgfjgjgf"));
 	}
 
 }
